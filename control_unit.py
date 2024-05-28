7ee7b12f802f037b85af3e2304956a4e22bd32ed
sh1kaMax <Shikmax2004@mail.ru>
2024-05-28 22:49:57 +0300

    finishing

--- control_unit.py
+++ control_unit.py
@@ -1,4 +1,5 @@
 from isa import Opcode
+import logging
 from signals import *
 from data_path import INSTRACTION_LIMIT
 from exceptions import InvalidSignalError
@@ -33,109 +34,126 @@ def opcode2microcode(opcode):
 
 microcode = [
     # Fetch next instraction - 0
-    [ARLatch.PC, MemorySignal.MemRead, MCAdrLatch.INC],
-    [IRLatch.MEM, MCAdrLatch.IR],
+    [ARLatch.PC, MEMSignal.READ, MCAdrLatch.INC],
+    [IRLatch.MEM, Instraction.INC, MCAdrLatch.IR],
     # SUM - 2
-    [ValueToALULatch.GetValues, AluLatch.SUM, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.SUM, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # SUB - 4
-    [ValueToALULatch.GetValues, AluLatch.SUB, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.SUB, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # MUL - 6
-    [ValueToALULatch.GetValues, AluLatch.MUL, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.MUL, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # DIV - 8
-    [ValueToALULatch.GetValues, AluLatch.DIV, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.DIV, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # MOD - 10
-    [ValueToALULatch.GetValues, AluLatch.MOD, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.MOD, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # DUP - 12
-    [DataStackSignals.Push, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # DROP - 14
-    [DataStackSignals.Pop, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Pop, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # SWAP - 16
     [BRLatch.DS, MCAdrLatch.INC],
-    [DataStackSignals.Push, TosLatch.BufferRegister, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, TosLatch.BR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # EQ - 19
-    [ValueToALULatch.GetValues, AluLatch.EQ, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.EQ, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # MORE - 21
-    [ValueToALULatch.GetValues, AluLatch.MORE, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.MORE, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # LESS - 23
-    [ValueToALULatch.GetValues, AluLatch.LESS, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.LESS, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # PUSH - 25
-    [DataStackSignals.Push, TosLatch.IR, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, TosLatch.IR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # ADR_ON_TOP - 27
-    [DataStackSignals.Push, TosLatch.IR, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, TosLatch.IR_VAR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # SAVE_VAR - 29
-    [ARLatch.TOS, BRLatch.DS, TosLatch.BufferRegister, MemorySignal.TOS, MCAdrLatch.INC],
-    [MemorySignal.MemWrite, BRLatch.DS, TosLatch.BufferRegister, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [ARLatch.TOS, BRLatch.DS, TosLatch.BR, MEMSignal.TOS, MCAdrLatch.INC],
+    [MEMSignal.WRITE, BRLatch.DS, TosLatch.BR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # VAR_ON_TOP - 32
-    [ARLatch.TOS, MemorySignal.MemRead, MCAdrLatch.INC],
+    [ARLatch.TOS, MEMSignal.READ, MCAdrLatch.INC],
     [IRLatch.MEM, TosLatch.IR, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # JZS - 35
-    [DataStackSignals.Push, TosLatch.IR, JUMPS.JZS, MCAdrLatch.INC],
-    [DataStackSignals.Pop, BRLatch.DS, TosLatch.BufferRegister, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, TosLatch.IR, JUMPS.JZS, MCAdrLatch.INC],
+    [DSLatch.Pop, BRLatch.DS, TosLatch.BR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # JMP - 38
-    [DataStackSignals.Push, TosLatch.IR, JUMPS.JMP, MCAdrLatch.INC],
-    [BRLatch.DS, TosLatch.BufferRegister, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, TosLatch.IR, JUMPS.JMP, MCAdrLatch.INC],
+    [BRLatch.DS, TosLatch.BR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # PRINT - 41
     [IOLatch.PRINT, MCAdrLatch.INC],
-    [BRLatch.DS, TosLatch.BufferRegister, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [BRLatch.DS, TosLatch.BR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # READ - 44
-    [DataStackSignals.Push, MCAdrLatch.INC],
-    [IOLatch.READ, PCLatch.INC, MCAdrLatch.ZERO],
+    [DSLatch.Push, MCAdrLatch.INC],
+    [IOLatch.READ, Instraction.INC, PCLatch.INC, MCAdrLatch.ZERO],
     # EMIT - 46
     [IOLatch.EMIT, MCAdrLatch.INC],
-    [BRLatch.DS, TosLatch.BufferRegister, MCAdrLatch.INC],
-    [PCLatch.INC, MCAdrLatch.ZERO],
+    [BRLatch.DS, TosLatch.BR, MCAdrLatch.INC],
+    [PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
     # HALT - 49
-    [PROG.HALT],
+    [Instraction.INC, PROG.HALT],
     # NOT_EQ - 50
-    [ValueToALULatch.GetValues, AluLatch.NOT_EQ, MCAdrLatch.INC],
-    [TosLatch.ALU, PCLatch.INC, MCAdrLatch.ZERO],
+    [ALUValues.VAR, AluLatch.NOT_EQ, MCAdrLatch.INC],
+    [TosLatch.ALU, PCLatch.INC, Instraction.INC, MCAdrLatch.ZERO],
 ]
 
 
 class ControlUnit:
     mcAdr = None
-    mcR = None
     datapath = None
     tick = None
+    instraction_count = None
 
     def __init__(self, datapath):
         self.mcAdr = 0
-        self.mcR = []
         self.datapath = datapath
         self.tick = 0
+        self.instraction_count = 0
+
+    def __repr__(self, signal):
+        state_repr = ("TICK: {:4} PC: {:3} ADDR: {:3} mcADDR: {:2} SIGNAL: {:15} TOS: {:6} Z: {:1} N: {:1} V: {:1}\n"
+                      "DS: {}").format(
+            str(self.tick),
+            str(self.datapath.pc),
+            str(self.datapath.address_register),
+            str(self.mcAdr),
+            str(signal),
+            str(self.datapath.top_of_stack) if self.datapath.top_of_stack is not None else '0',
+            str(self.datapath.alu.z_flag),
+            str(self.datapath.alu.n_flag),
+            str(self.datapath.alu.v_flag),
+            self.datapath.data_stack.stack
+        )
+        return state_repr
 
     def inc_tick(self):
         self.tick += 1
+
 
     def execute_instraction(self, signals):
         for signal in signals:
             if isinstance(signal, ARLatch):
                 self.datapath.address_register_latch(signal)
-            elif isinstance(signal, MemorySignal):
+            elif isinstance(signal, MEMSignal):
                 self.datapath.memory_latch(signal)
             elif isinstance(signal, MCAdrLatch):
                 self.mcAdr_latch(signal)
             elif isinstance(signal, IRLatch):
                 self.datapath.mem_value_to_ir()
-            elif isinstance(signal, ValueToALULatch):
+            elif isinstance(signal, ALUValues):
                 self.datapath.alu_values_get()
             elif isinstance(signal, AluLatch):
                 self.datapath.alu_latch(signal)
@@ -143,7 +161,7 @@ class ControlUnit:
                 self.datapath.tos_latch(signal)
             elif isinstance(signal, PCLatch):
                 self.datapath.pc_latch(signal)
-            elif isinstance(signal, DataStackSignals):
+            elif isinstance(signal, DSLatch):
                 self.datapath.data_stack_latch(signal)
             elif isinstance(signal, BRLatch):
                 self.datapath.write_buffer_register()
@@ -151,10 +169,14 @@ class ControlUnit:
                 self.datapath.io_latch(signal)
             elif isinstance(signal, JUMPS):
                 self.datapath.jump(signal)
+            elif isinstance(signal, Instraction):
+                self.instraction_count += 1
             elif isinstance(signal, PROG):
                 raise StopIteration
             else:
                 raise InvalidSignalError(signal)
+            logging.debug("%s", self.__repr__(signal))
+            self.inc_tick()
 
     def mcAdr_latch(self, signal):
         match signal:
@@ -166,14 +188,15 @@ class ControlUnit:
                 self.mcAdr = 0
 
     def run_machine(self):
-        instraction_counter = 0
         try:
-            while instraction_counter < INSTRACTION_LIMIT:
+            while self.instraction_count < INSTRACTION_LIMIT:
                 self.execute_instraction(microcode[self.mcAdr])
-                instraction_counter += 1
         except StopIteration:
             pass
         except IOError:
             pass
-
-        print("output_buffer:", repr("".join(self.datapath.output_buffer)))
+        output = ""
+        for stroka in ''.join(self.datapath.output_buffer).split("\n"):
+            output += f'{4 * '\t'}{stroka}\n'
+        logging.debug("output_buffer: \n" + output[0:-1])
+        return self.datapath.output_buffer, self.instraction_count, self.tick
